13da37e472b9d49863ba74cc7e7654dad43b69d1
Dexied <33525453+Dexied@users.noreply.github.com>
2025-11-23 02:34:19 +0500

    570-001

--- PluginBuild.bat
+++ PluginBuild.bat
@@ -1,1 +1,4 @@
-B:\UnrealEngineSource\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\XAIDemo\Plugins\XAI\XAI.uplugin" -Package="B:\Projects\XAIDemo\Builds\XAI" -Rocket -2019
+@echo off
+B:\UnrealEngineSource\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\XAIDemo\Plugins\XAI\XAI.uplugin" -Package="B:\Projects\XAIDemo\Builds\XAI" -Rocket -2019 > "B:\Projects\XAIDemo\BuildLog.txt" 2>&1
+echo Build completed. Check BuildLog.txt for details.
+pause
--- Source/XAIDemoEditor.Target.cs
+++ Source/XAIDemoEditor.Target.cs
@@ -9,7 +9,7 @@ public class XAIDemoEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+        	IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
         ExtraModuleNames.AddRange( new string[] { "XAIDemo" } );
 	}
