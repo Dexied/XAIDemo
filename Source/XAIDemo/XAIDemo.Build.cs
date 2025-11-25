4904e13f1bdc7558779fe5ca6994a8ccd1629b70
Dexied <33525453+Dexied@users.noreply.github.com>
2025-11-26 02:00:12 +0500

    570-003

--- Source/XAIDemo/XAIDemo.Build.cs
+++ Source/XAIDemo/XAIDemo.Build.cs
@@ -10,7 +10,7 @@ public class XAIDemo : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "XAI" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
